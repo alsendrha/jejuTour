719cf4e6ddab49123c47d6a3a92bc2309fc0c769
Minyoung <alsendrha@naver.com>
2025-03-04 16:23:58 +0900

    지도 수정

--- src/app/main/[id]/page.tsx
+++ src/app/main/[id]/page.tsx
@@ -2,52 +2,33 @@
 
 import { useGetDetailData } from "@/api";
 import DetailImage from "@/components/detail/DetailImage";
+import DetailMap from "@/components/detail/DetailMap";
 import DetailTag from "@/components/detail/DetailTag";
-import { GoogleMap, LoadScriptNext, Marker } from "@react-google-maps/api";
+
 import { usePathname } from "next/navigation";
 
 const DetailContent = () => {
   const name = usePathname().split("/")[2];
   const { data, isLoading } = useGetDetailData(name);
+
   console.log(data);
   if (isLoading) return <div>Loading...</div>;
   return (
     <div className="w-full flex justify-center pt-[90px]">
       <div className="w-[1000px] flex flex-col">
-        {data?.map((item) => (
-          <div key={item.contentsid}>
-            <div className="w-full text-center">
-              <p>{item.title}</p>
+        {data?.map((item) => {
+          return (
+            <div key={item.contentsid}>
+              <div className="w-full text-center">
+                <p>{item.title}</p>
+              </div>
+              <DetailImage image={item.repPhoto.photoid.imgpath} />
+              <p>{item.introduction}</p>
+              <DetailTag item={item} />
+              <DetailMap item={item} />
             </div>
-            <DetailImage image={item.repPhoto.photoid.imgpath} />
-            <p>{item.introduction}</p>
-            <DetailTag item={item} />
-            <div className="w-[600px] h-[600px] rounded-full overflow-hidden">
-              <LoadScriptNext
-                googleMapsApiKey={`${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
-              >
-                <GoogleMap
-                  mapContainerStyle={{
-                    width: "100%",
-                    height: "100%",
-                  }}
-                  center={{
-                    lat: Number(item.latitude),
-                    lng: Number(item.longitude),
-                  }}
-                  zoom={18}
-                >
-                  <Marker
-                    position={{
-                      lat: Number(item.latitude),
-                      lng: Number(item.longitude),
-                    }}
-                  />
-                </GoogleMap>
-              </LoadScriptNext>
-            </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/components/detail/DetailMap.tsx
@@ -0,0 +1,56 @@
+import { JeJuData } from "@/types/type";
+import {
+  Circle,
+  GoogleMap,
+  LoadScriptNext,
+  Marker,
+} from "@react-google-maps/api";
+
+type DetailMapProps = {
+  item: JeJuData;
+};
+
+const DetailMap = ({ item }: DetailMapProps) => {
+  const options = {
+    strokeColor: "#FFE600",
+    strokeOpacity: 0.3,
+    strokeWeight: 1,
+    fillColor: "#FFE600",
+    fillOpacity: 0.3,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    visible: true,
+    radius: 80,
+    zIndex: 1,
+  };
+  const position = {
+    lat: Number(item.latitude),
+    lng: Number(item.longitude),
+  };
+  return (
+    <div className="w-[600px] h-[600px] rounded-full overflow-hidden">
+      <LoadScriptNext
+        googleMapsApiKey={`${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`}
+      >
+        <GoogleMap
+          options={{
+            panControl: false,
+            mapTypeControl: false,
+          }}
+          mapContainerStyle={{
+            width: "100%",
+            height: "100%",
+          }}
+          center={position}
+          zoom={18}
+        >
+          <Marker position={position} />
+          <Circle center={position} options={options} />
+        </GoogleMap>
+      </LoadScriptNext>
+    </div>
+  );
+};
+
+export default DetailMap;
